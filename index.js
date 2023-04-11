9de0aef2930fb7cca3b346d704358d253fbe1333
Igor Pekovic <igor.pekovic95@gmail.com>
2023-04-11 09:58:27 +0200

    Push

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const form = document.querySelector("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+const customerName = document.getElementById("customerName");
+const customerEmail = document.getElementById("customerEmail");
+const customerAddress = document.getElementById("customerAddress");
+const customerHomeNumber = document.getElementById("customerHomeNumber");
+const customerZipcode = document.getElementById("customerZipcode");
+const customerPhoneNumber = document.getElementById("customerPhoneNumber");
+const customerLocation = document.getElementById("customerLocation");
+
 const submitBtn = document.querySelector("button");
 
 form.addEventListener("submit", (e) => {
@@ -11,29 +15,81 @@ form.addEventListener("submit", (e) => {
   checkInputs();
 });
 
-function checkInputs() {
-  const usernameValue = username.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
-  const password2Value = password2.value.trim();
+// function checkInputs() {
+//   const usernameValue = customerName.value.trim();
+//   const emailValue = customerEmail.value.trim();
+//   const addressValue = customerAddress.value.trim();
+//   const homeNumberValue = customerHomeNumber.value.trim();
+//   const zipcodeValue = customerZipcode.value.trim();
+//   const phoneNumberValue = customerPhoneNumber.value.trim().replaceAll("-", "");
+//   const locationValue = customerLocation.value.trim();
 
-  if (usernameValue === "") setErrorFor(username, "Username cannot be blank");
-  if (usernameValue !== "") setSuccessFor(username, "Dobri podaci");
+//   if (usernameValue === "")
+//     setErrorFor(customerName, "Molimo unesite ime i prezime");
+//   if (usernameValue !== "") setSuccessFor(customerName);
 
-  if (emailValue === "") setErrorFor(email, "Email cannot be blank");
-  if (emailValue !== "" && !isEmail(emailValue))
-    setErrorFor(email, "Not a valid email");
-  if (emailValue !== "" && isEmail(emailValue)) setSuccessFor(email);
+//   if (emailValue === "")
+//     setErrorFor(customerEmail, "Molimo unesite email adresu");
+//   if (emailValue !== "" && !isEmail(emailValue))
+//     setErrorFor(customerEmail, "Molimo unesite pravilan email");
+//   if (emailValue !== "" && isEmail(emailValue)) setSuccessFor(customerEmail);
 
-  if (passwordValue === "") setErrorFor(password, "Password cannot be blank");
-  if (passwordValue !== "") setSuccessFor(password);
+//   if (addressValue === "")
+//     setErrorFor(customerAddress, "Molimo unesite adresu");
+//   if (addressValue !== "") setSuccessFor(customerAddress);
 
-  if (password2Value === "")
-    setErrorFor(password2, "Password2 cannot be blank");
-  if (password2Value !== "" && passwordValue !== password2Value)
-    setErrorFor(password2, "Passwords does not match");
-  if (password2Value !== "" && passwordValue === password2Value)
-    setSuccessFor(password2);
+//   if (homeNumberValue === "")
+//     setErrorFor(customerHomeNumber, "Molimo unesite kućni broj");
+//   if (homeNumberValue !== "") setSuccessFor(customerHomeNumber);
+
+//   if (zipcodeValue === "")
+//     setErrorFor(customerZipcode, "Molimo unesite poštanski broj");
+//   if (zipcodeValue !== "") setSuccessFor(customerZipcode);
+
+//   if (phoneNumberValue === "")
+//     setErrorFor(customerPhoneNumber, "Molimo unesite telefonski broj");
+//   if (phoneNumberValue !== "" && phoneNumberValue.length < 9)
+//     setErrorFor(customerPhoneNumber, "Broj mora sadržati najmanje 9 brojeva");
+//   if (phoneNumberValue !== "" && phoneNumberValue.length > 9)
+//     setSuccessFor(customerPhoneNumber);
+
+//   if (locationValue === "")
+//     setErrorFor(customerLocation, "Molimo unesite mjesto");
+//   if (locationValue !== "") setSuccessFor(customerLocation);
+// }
+
+// function setErrorFor(input, message) {
+//   const formInput = input.parentElement;
+//   const errorEl = formInput.querySelector(".error-message");
+//   formInput.className = "form-input error";
+//   errorEl.innerText = message;
+// }
+
+// function setSuccessFor(input) {
+//   const formInput = input.parentElement;
+//   formInput.className = "form-input success";
+// }
+
+// function isEmail(email) {
+//   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+//     email
+//   );
+// }
+
+function validateInput(
+  inputValue,
+  errorMessage,
+  inputElement,
+  validationFunction,
+  validationErrorMessage
+) {
+  if (inputValue === "") {
+    setErrorFor(inputElement, errorMessage);
+  } else if (validationFunction && !validationFunction(inputValue)) {
+    setErrorFor(inputElement, validationErrorMessage || errorMessage);
+  } else {
+    setSuccessFor(inputElement);
+  }
 }
 
 function setErrorFor(input, message) {
@@ -43,15 +99,77 @@ function setErrorFor(input, message) {
   errorEl.innerText = message;
 }
 
-function setSuccessFor(input, message) {
+function setSuccessFor(input) {
   const formInput = input.parentElement;
-  const successEl = formInput.querySelector(".success-message");
   formInput.className = "form-input success";
-  successEl.innerText = message;
 }
 
 function isEmail(email) {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     email
   );
+}
+
+function checkInputs() {
+  const nameValue = customerName.value.trim();
+  const emailValue = customerEmail.value.trim();
+  const addressValue = customerAddress.value.trim();
+  const homeNumberValue = customerHomeNumber.value.trim();
+  const zipcodeValue = customerZipcode.value.trim();
+  const phoneNumberValue = customerPhoneNumber.value.trim().replaceAll(" ", "");
+  const locationValue = customerLocation.value.trim();
+
+  validateInput(nameValue, "Molimo unesite ime i prezime", customerName, null);
+
+  validateInput(
+    emailValue,
+    "Molimo unesite ispravnu email adresu",
+    customerEmail,
+    isEmail
+  );
+
+  validateInput(addressValue, "Molimo unesite adresu", customerAddress, null);
+
+  validateInput(
+    homeNumberValue,
+    "Molimo unesite kućni broj",
+    customerHomeNumber,
+    null
+  );
+
+  validateInput(
+    zipcodeValue,
+    "Molimo unesite poštanski broj",
+    customerZipcode,
+    null
+  );
+
+  validateInput(
+    phoneNumberValue,
+    "Molimo unesite telefonski broj",
+    customerPhoneNumber,
+    (phoneNumber) => phoneNumber.length >= 9,
+    "Broj mora sadržati najmanje 9 brojeva"
+  );
+
+  validateInput(locationValue, "Molimo unesite mjesto", customerLocation, null);
+
+  const formInputs = document.querySelectorAll(".form-input");
+  let isValid = true;
+  formInputs.forEach((input) => {
+    if (input.classList.contains("error")) isValid = false;
+  });
+
+  // if all inputs are valid, log the input values
+  if (isValid) {
+    console.log(
+      `Ime i prezime: ${nameValue}
+        Email: ${emailValue}
+        Adresa: ${addressValue}
+        Kućni broj: ${homeNumberValue}
+        Postanski broj: ${zipcodeValue}
+        Telefonski broj: ${phoneNumberValue}
+        Mjesto: ${locationValue}`
+    );
+  }
 }
